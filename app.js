7899d46d9d2160dda6c862358595878258a00211
Konstantin Chvykov <tanti_2301@hotmail.com>
2026-03-19 15:51:29 +0900

    step

---
+++ app.js
@@ -0,0 +1,32 @@
+const LIFF_ID = "PASTE_YOUR_LIFF_ID_HERE";
+
+async function init() {
+  await liff.init({ liffId: LIFF_ID });
+
+  if (!liff.isLoggedIn()) {
+    liff.login();
+  }
+
+  const profile = await liff.getProfile();
+  console.log("User:", profile);
+}
+
+init();
+
+function goBooking() {
+  document.querySelector(".active").classList.remove("active");
+  document.getElementById("booking").classList.add("active");
+}
+
+function goForm() {
+  document.querySelector(".active").classList.remove("active");
+  document.getElementById("form").classList.add("active");
+}
+
+function confirmBooking() {
+  alert("予約完了！");
+}
+
+function submitForm() {
+  alert("送信完了！");
+}
