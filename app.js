1e0bf99fec7e0a2a876ab78d3b1a948dd55ce7e0
Konstantin Chvykov <tanti_2301@hotmail.com>
2026-03-19 16:22:15 +0900

    step3

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const LIFF_ID = "2009520436-mnlGZ5e";
 async function init() {
   await liff.init({ liffId: LIFF_ID });
 
-  if (!liff.isLoggedIn()) {
-    liff.login();
-  }
+ // if (!liff.isLoggedIn()) {
+  //  liff.login();
+ // }
 
   const profile = await liff.getProfile();
   console.log("User:", profile);
