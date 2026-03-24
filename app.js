49a67560a1486639fde957e74cef8351060b12be
Konstantin Chvykov <tanti_2301@hotmail.com>
2026-03-24 14:49:08 +0900

    step7

--- app.js
+++ app.js
@@ -1,40 +1,101 @@
-const LIFF_ID = "2009534336-E6S4sA5p";
+const LIFF_ID = "ТВОЙ_LIFF_ID";
 
 async function init() {
   try {
     await liff.init({ liffId: LIFF_ID });
 
-    // ❌ УБИРАЕМ login
-    // if (!liff.isLoggedIn()) {
-    //   liff.login();
-    // }
-
     if (liff.isLoggedIn()) {
       const profile = await liff.getProfile();
       console.log("User:", profile);
     }
-
   } catch (e) {
-    console.log("LIFF init error:", e);
+    console.log("LIFF error:", e);
   }
 }
 
 init();
 
+function showScreen(id) {
+  document.querySelectorAll(".screen").forEach((el) => {
+    el.classList.remove("active");
+  });
+  document.getElementById(id).classList.add("active");
+}
+
+function goWelcome() {
+  showScreen("welcome");
+}
+
 function goBooking() {
-  document.querySelector(".active").classList.remove("active");
-  document.getElementById("booking").classList.add("active");
+  showScreen("booking");
 }
 
 function goForm() {
-  document.querySelector(".active").classList.remove("active");
-  document.getElementById("form").classList.add("active");
+  showScreen("form");
 }
 
-function confirmBooking() {
-  alert("予約完了！");
+function selectService(service, button) {
+  document.getElementById("service").value = service;
+
+  document.querySelectorAll(".service-card").forEach((el) => {
+    el.classList.remove("active-service");
+  });
+
+  button.classList.add("active-service");
+}
+
+
+
+function finishBooking() {
+  alert("予約が確定しました！");
+  goWelcome();
 }
 
 function submitForm() {
-  alert("送信完了！");
+  const name = document.getElementById("name").value.trim();
+  const phone = document.getElementById("phone").value.trim();
+
+  const service = document.getElementById("service").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+
+  if (!name || !phone) {
+    alert("名前と電話番号を入力してください");
+    return;
+  }
+
+  fetch("https://script.google.com/macros/s/AKfycbydzIJaGwpO3p9rrDO5D9y10_OvrqB45nwaHtZ62OFj3mNFDCJbCHn04YqcpS3HyhB9/exec", {
+    method: "POST",
+    body: JSON.stringify({
+      name: name,
+      phone: phone,
+      service: service,
+      date: date,
+      time: time
+    })
+  })
+  .then(() => {
+    alert("送信完了！");
+    goWelcome();
+  })
+  .catch(() => {
+    alert("送信エラー");
+  });
+}
+
+function goConfirm() {
+  const service = document.getElementById("service").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+
+  if (!service || !date || !time) {
+    alert("サービス・日付・時間を選択してください");
+    return;
+  }
+
+  document.getElementById("confirmService").textContent = service;
+  document.getElementById("confirmDate").textContent = date;
+  document.getElementById("confirmTime").textContent = time;
+
+  showScreen("confirm");
 }
