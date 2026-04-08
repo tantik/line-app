385d5635c28821988e2e27e97e2cc24336ae1bbf
Konstantin Chvykov <tanti_2301@hotmail.com>
2026-04-08 19:00:01 +0900

    54

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   LIFF_ID: "2009586903-hyNXZaW7",
-  WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbxThHxrZ3Fifn4EzyHED9kZ9KBMIQMDDRVwvdG5S89QO1B255eYjeCBGaElMFWdnwzj/exec",
+  WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbx6lGm1eXiT3Fvue5E10BJXcArGhQnLyqx7SgzYAbJDtSCGkg_BjcDprd64-i0Ig-Dl/exec",
   BUSINESS_LABEL: "Salon",
   DATE_RANGE_DAYS: 60,
   INITIAL_VISIBLE_DAYS: 14,
